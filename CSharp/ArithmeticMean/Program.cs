b2f11ff86975f7321eec20943ae9d82d8cab4fdf
Olga <ol_0524@mail.ru>
2022-10-01 22:24:51 +0300

    1

---
+++ CSharp/ArithmeticMean/Program.cs
@@ -0,0 +1,19 @@
+﻿// Среднее арифметическое двух чисел  
+
+double ferstValue, secondValue;
+
+Console.WriteLine("Введите первое число: ");
+ferstValue = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+secondValue = double.Parse(Console.ReadLine());
+
+double result = (ferstValue + secondValue) / 2;
+
+Console.WriteLine("Среднее арифметическое двух чисел: " + result);  
+
+
+Console.WriteLine();
+
+
+
---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿// Введите 3 числа и выведите значение суммы и произведения этих чисел
+
+double ferstnumber, secondnumber, thirdnumber;
+
+Console.WriteLine("Введите первое число: ");
+ferstnumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+secondnumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+thirdnumber = double.Parse(Console.ReadLine());
+
+double sum = ferstnumber + secondnumber + thirdnumber;
+double product = ferstnumber * secondnumber * thirdnumber;
+
+Console.WriteLine("Сумма трех чисел: " + sum);
+Console.WriteLine("Произведение трех чисел: " + product);
+
+Console.WriteLine();
